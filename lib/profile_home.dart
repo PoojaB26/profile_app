87b40416e61998fa4434ca7b409bd10f41d3a8b5
PoojaB26 <pbhaumik26@gmail.com>
2019-02-07 22:54:37 +0530

    action items for profile-home-appbar

--- lib/profile_home.dart
+++ lib/profile_home.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ProfileHome extends StatelessWidget {
+  final _backgroundColor = Color(0xff020021);
+
+  final _imageUrl =
+      'https://pbs.twimg.com/profile_images/947228834121658368/z3AHPKHY_400x400.jpg';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Profile Home'), actions: <Widget>[
+      backgroundColor: _backgroundColor,
+      appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: _backgroundColor,
+          actions: <Widget>[
         IconButton(
-          onPressed: null,
+          onPressed: () {},
           icon: Icon(Icons.favorite_border),
         ),
         IconButton(
-          onPressed: null,
+          onPressed: () {},
           icon: Icon(Icons.more_vert),
         )
       ]),
+      body: Container(),
     );
   }
 }
