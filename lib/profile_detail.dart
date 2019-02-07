01bd0e8ecb3b43dbbd507c6983e2d22f70d0f946
PoojaB26 <pbhaumik26@gmail.com>
2019-02-07 20:13:43 +0530

    UI for profile-detail

--- lib/profile_detail.dart
+++ lib/profile_detail.dart
@@ -6,17 +6,34 @@ class ProfileDetail extends StatelessWidget {
       'https://pbs.twimg.com/profile_images/947228834121658368/z3AHPKHY_400x400.jpg';
 
 
+  final _hiredTextStyle = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Hero(
-          tag: 'eric',
-          child: CircleAvatar(
-            maxRadius: 100.0,
-            backgroundImage: NetworkImage(_imageUrl),
-          ),
+      appBar: AppBar(
+        title: Text('Hired'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(50.0),
+       // color: Colors.yellowAccent,
+        width: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Hero(
+              tag: 'eric',
+              child: CircleAvatar(
+                maxRadius: 100.0,
+                backgroundImage: NetworkImage(_imageUrl),
+              ),
+            ),
+            Text('Candidate is Hired', style: _hiredTextStyle,)
+          ],
         ),
       ),
     );
