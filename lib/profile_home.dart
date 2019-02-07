acde05cf17c693dae1dbaf51246925c3074e4e16
PoojaB26 <pbhaumik26@gmail.com>
2019-02-08 00:15:16 +0530

    section 3 : favButton logic

--- lib/profile_home.dart
+++ lib/profile_home.dart
@@ -1,112 +1,119 @@
 import 'package:flutter/material.dart';
 
-class ProfileHome extends StatelessWidget {
+class ProfileHome extends StatefulWidget {
+  @override
+  ProfileHomeState createState() {
+    return ProfileHomeState();
+  }
+}
+
+class ProfileHomeState extends State<ProfileHome> {
   final _backgroundColor = Color(0xff020021);
 
   final _imageUrl =
       'https://pbs.twimg.com/profile_images/947228834121658368/z3AHPKHY_400x400.jpg';
 
   get _devName => Text(
-        'Eric Seidel',
-        style: TextStyle(
-            color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.bold),
-      );
+    'Eric Seidel',
+    style: TextStyle(
+        color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.bold),
+  );
 
   get _profileImage => Stack(
-        children: <Widget>[
-          CircleAvatar(
-            maxRadius: 50.0,
-            backgroundImage: NetworkImage(_imageUrl),
+    children: <Widget>[
+      CircleAvatar(
+        maxRadius: 50.0,
+        backgroundImage: NetworkImage(_imageUrl),
+      ),
+      Positioned(
+        bottom: 0.0,
+        right: 0.0,
+        child: Container(
+          height: 30.0,
+          width: 30.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(200.0),
+            color: Colors.blue,
           ),
-          Positioned(
-            bottom: 0.0,
-            right: 0.0,
-            child: Container(
-              height: 30.0,
-              width: 30.0,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(200.0),
-                color: Colors.blue,
-              ),
-              child: Icon(Icons.check, color: Colors.white, size: 18.0,),
-            ),
-          )
-        ],
-      );
+          child: Icon(Icons.check, color: Colors.white, size: 18.0,),
+        ),
+      )
+    ],
+  );
 
   get _devRole => Text(
-        'ENGINEERING MANAGER',
-        style: TextStyle(
-            color: Colors.grey[400],
-            fontSize: 16.0,
-            fontWeight: FontWeight.w500),
-      );
+    'ENGINEERING MANAGER',
+    style: TextStyle(
+        color: Colors.grey[400],
+        fontSize: 16.0,
+        fontWeight: FontWeight.w500),
+  );
 
   get _devDescription => Text(
-        'Co-founded the Flutter Project',
-        style: TextStyle(
-            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w500),
-      );
+    'Co-founded the Flutter Project',
+    style: TextStyle(
+        color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w500),
+  );
 
   get _socialLinks => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Image.asset(
-            'assets/linkedin.png',
-            height: 20.0,
-          ),
-          Image.asset(
-            'assets/twitter.png',
-            height: 20.0,
-          ),
-          Image.asset(
-            'assets/skype.png',
-            height: 20.0,
-          )
-        ],
-      );
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      Image.asset(
+        'assets/linkedin.png',
+        height: 20.0,
+      ),
+      Image.asset(
+        'assets/twitter.png',
+        height: 20.0,
+      ),
+      Image.asset(
+        'assets/skype.png',
+        height: 20.0,
+      )
+    ],
+  );
 
   get _launchWebsiteButton => OutlineButton(
-        onPressed: () {},
-        child: Text(
-          'LAUNCH WEBSITE',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 14.0,
-          ),
-        ),
-      );
+    onPressed: () {},
+    child: Text(
+      'LAUNCH WEBSITE',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 14.0,
+      ),
+    ),
+  );
 
   get _hireMeButton => FlatButton(
-        onPressed: () {},
-        padding: EdgeInsets.symmetric(horizontal: 35.0),
-        color: Colors.blue,
-        child: Text(
-          'HIRE ME',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
-          ),
-        ),
-      );
+    onPressed: () {},
+    padding: EdgeInsets.symmetric(horizontal: 35.0),
+    color: Colors.blue,
+    child: Text(
+      'HIRE ME',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+      ),
+    ),
+  );
 
   get _devTags => Container(
-        height: 50.0,
-        //color: Colors.white,
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            getTag('Flutter'),
-            getTag('Dart'),
-            getTag('Android'),
-            getTag('iOS'),
-            getTag('React Native'),
-            getTag('Material Design'),
-          ],
-        ),
-      );
+    height: 50.0,
+    //color: Colors.white,
+    child: ListView(
+      physics: BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        getTag('Flutter'),
+        getTag('Dart'),
+        getTag('Android'),
+        getTag('iOS'),
+        getTag('React Native'),
+        getTag('Material Design'),
+      ],
+    ),
+  );
 
   Padding getTag(tagName) {
     return Padding(
@@ -121,6 +128,18 @@ class ProfileHome extends StatelessWidget {
     );
   }
 
+
+  bool isFavourite = false;
+
+  _toggleFavButton(){
+    setState(() {
+      isFavourite = !isFavourite;
+    });
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -130,8 +149,8 @@ class ProfileHome extends StatelessWidget {
           backgroundColor: _backgroundColor,
           actions: <Widget>[
             IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.favorite_border),
+              onPressed: _toggleFavButton,
+              icon: isFavourite ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
             ),
             IconButton(
               onPressed: () {},
