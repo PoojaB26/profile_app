bfdb33ff97b6fb5b5c0f8a2c480329bb739ac842
PoojaB26 <pbhaumik26@gmail.com>
2019-02-07 22:26:51 +0530

    profile-detail UI

--- lib/profile_details.dart
+++ lib/profile_details.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ProfileDetail extends StatelessWidget {
+
+  final _backgroundColor = Color(0xff020021);
+
+  final _imageUrl = 'https://pbs.twimg.com/profile_images/947228834121658368/z3AHPKHY_400x400.jpg';
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
+        backgroundColor: _backgroundColor,
         title: Text('Profile Details'),
+      ),
+      body: Container(
+        //color: Colors.grey,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              CircleAvatar(
+                maxRadius: 80.0,
+                backgroundImage: NetworkImage(_imageUrl),
+              ),
+              Text('Hired!', style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+              ),)
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/profile_home.dart
+++ lib/profile_home.dart
@@ -4,9 +4,16 @@ class ProfileHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile Home'),
-      ),
+      appBar: AppBar(title: Text('Profile Home'), actions: <Widget>[
+        IconButton(
+          onPressed: null,
+          icon: Icon(Icons.favorite_border),
+        ),
+        IconButton(
+          onPressed: null,
+          icon: Icon(Icons.more_vert),
+        )
+      ]),
     );
   }
 }
